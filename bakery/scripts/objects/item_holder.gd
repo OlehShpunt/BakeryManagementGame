44a00878789db04b9da5f26070e83978c2aae563
OlehShpunt <olehshpunt2@gmail.com>
2025-02-20 13:13:59 -0800

    Minor ItemHolder fixes.
    Eliminated the problem of duping items when trying to add item to the holder that has already held an item.

--- bakery/scripts/objects/item_holder.gd
+++ bakery/scripts/objects/item_holder.gd
@@ -18,19 +18,20 @@ func set_item(item_to_add : String):
 		push_error("The texture already holds an item")
 
 func _input(event: InputEvent) -> void:
+	## Pick item
 	# When hover over cell and left click, you pick the item and move it.
 	if event.is_action_pressed("left_click") and mouse_is_hovering and current_item_string != "":
 		print(">>>>> Picking the ", current_item_string)
 		var item_scene = scene_manager.get_packed_scene(current_item_string)
 		add_child(item_scene.instantiate())
 		empty_cell() # remove the texture from this cell
-		# Access the last child - the floating item. 
-		# When is_being_moved_by_mouse true, the global pos is same as mouse's gl pos
+		# Access the last child - the floating item. When is_being_moved_by_mouse true, the global pos is same as mouse's gl pos
 		get_child(-1).is_being_moved_by_mouse = true
 		get_child(-1).z_index = 15
 
 func empty_cell():
 	texture.set_texture(null)
+	current_item_string = ""
 
 func _on_cell_mouse_entered() -> void:
 	print(">>>>mouse entered")
