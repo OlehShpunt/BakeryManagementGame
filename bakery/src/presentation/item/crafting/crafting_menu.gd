23d34dc3e27ff6e753f5f7b1b6adbe7b205bb42a
OlehShpunt <olehshpunt2@gmail.com>
2026-05-23 23:55:04 +0300

    feat: load recipes to crafting menu

---
+++ bakery/src/presentation/item/crafting/crafting_menu.gd
@@ -0,0 +1,132 @@
+extends Control
+
+const crafting_menu_row_preload: PackedScene = preload("res://src/presentation/item/crafting/crafting_menu_row.tscn")
+@onready var rows_container: VBoxContainer = $PanelContainer/VBoxContainer/MarginContainer2/ScrollContainer/VBoxContainer
+
+
+func _ready() -> void:
+	for item_code: int in EnumHolder.ItemCode.values():
+		print("bams ", item_code)
+		var recipeItemCodes: Array[EnumHolder.ItemCode] = Recipes.get_recipe_by_item_code(item_code)
+		var recipe: Array[Item] = []
+		for code in recipeItemCodes:
+			recipe.append(Item.new(code))
+
+		var result_item: Item = Item.new(item_code)
+		var result: Dictionary = CraftingMenuRow.create(recipe, result_item)
+
+		if (result.get("success")):
+			var crafting_menu_row_instance: CraftingMenuRow = result.get("row")
+			rows_container.add_child(crafting_menu_row_instance)
+		else:
+			pass
+
+
+class Recipe:
+	var name: String
+	var ingredients: Array
+	var result: String
+
+# func get_image_path_for_item(item_key: String) -> String:
+# 	match item_key:
+# 		"flour":
+# 			return PathHolder.FLOUR_IMAGE
+# 		"milk":
+# 			return PathHolder.MILK_IMAGE
+# 		"butter":
+# 			return PathHolder.BUTTER_IMAGE
+# 		"chocolate":
+# 			return PathHolder.CHOCOLATE_IMAGE
+# 		"vanilla":
+# 			return PathHolder.VANILLA_IMAGE
+# 		"cocoa_powder":
+# 			return PathHolder.COCOA_IMAGE
+# 		"nuts":
+# 			return PathHolder.NUTS_IMAGE
+# 		"cherry":
+# 			return PathHolder.CHERRY_IMAGE
+# 		"jello":
+# 			return PathHolder.JELLO_IMAGE
+# 		"bread":
+# 			return PathHolder.BREAD_IMAGE
+# 		"bagel":
+# 			return PathHolder.BAGEL_IMAGE
+# 		"waffle":
+# 			return PathHolder.WAFFLE_IMAGE
+# 		"sponge_cake":
+# 			return PathHolder.SPONGE_CAKE_IMAGE
+# 		"donut":
+# 			return PathHolder.DONUT_IMAGE
+# 		"chocolate_candy":
+# 			return PathHolder.CHOCOLATE_CANDY_IMAGE
+# 		"signature_chocolate":
+# 			return PathHolder.SIGNATURE_CHOCOLATE_IMAGE
+# 		"pudding":
+# 			return PathHolder.PUDDING_IMAGE
+# 		"chocolate_bun":
+# 			return PathHolder.CHOCOLATE_BUN_IMAGE
+# 		"muffin":
+# 			return PathHolder.MUFFIN_IMAGE
+# 		"nut_candy":
+# 			return PathHolder.NUT_CANDY_IMAGE
+# 		"cookie":
+# 			return PathHolder.COOKIE_IMAGE
+# 		"nut_cake":
+# 			return PathHolder.NUT_CAKE_IMAGE
+# 		"cherry_cake":
+# 			return PathHolder.CHERRY_CAKE_IMAGE
+# 		_:
+# 			push_warning("Unknown item key: " + item_key)
+# 			return ""
+
+# func get_item_code_for_item(item_key: String) -> EnumHolder.ItemCode:
+# 	match item_key:
+# 		"flour":
+# 			return EnumHolder.ItemCode.Flour
+# 		"milk":
+# 			return EnumHolder.ItemCode.Milk
+# 		"butter":
+# 			return EnumHolder.ItemCode.Butter
+# 		"chocolate":
+# 			return EnumHolder.ItemCode.Chocolate
+# 		"vanilla":
+# 			return EnumHolder.ItemCode.Vanilla
+# 		"cocoa_powder":
+# 			return EnumHolder.ItemCode.CocoaPowder
+# 		"nuts":
+# 			return EnumHolder.ItemCode.Nuts
+# 		"cherry":
+# 			return EnumHolder.ItemCode.Cherry
+# 		"jello":
+# 			return EnumHolder.ItemCode.Jello
+# 		"bread":
+# 			return EnumHolder.ItemCode.Bread
+# 		"bagel":
+# 			return EnumHolder.ItemCode.Bagel
+# 		"waffle":
+# 			return EnumHolder.ItemCode.Waffle
+# 		"sponge_cake":
+# 			return EnumHolder.ItemCode.SpongeCake
+# 		"donut":
+# 			return EnumHolder.ItemCode.Donut
+# 		"chocolate_candy":
+# 			return EnumHolder.ItemCode.ChocolateCandy
+# 		"signature_chocolate":
+# 			return EnumHolder.ItemCode.SignatureChocolate
+# 		"pudding":
+# 			return EnumHolder.ItemCode.Pudding
+# 		"chocolate_bun":
+# 			return EnumHolder.ItemCode.ChocolateBun
+# 		"muffin":
+# 			return EnumHolder.ItemCode.Muffin
+# 		"nut_candy":
+# 			return EnumHolder.ItemCode.NutCandy
+# 		"cookie":
+# 			return EnumHolder.ItemCode.Cookie
+# 		"nut_cake":
+# 			return EnumHolder.ItemCode.NutCake
+# 		"cherry_cake":
+# 			return EnumHolder.ItemCode.CherryCake
+# 		_:
+# 			push_warning("Unknown item key: " + item_key)
+# 			return -1
---
+++ bakery/src/presentation/item/crafting/crafting_menu_row.gd
@@ -0,0 +1,190 @@
+class_name CraftingMenuRow
+extends Control
+
+@onready var item1_name_label: Label = $HBoxContainer/Item1/PanelContainer/Panel/VBoxContainer/ItemName
+@onready var item1_texture_rect: TextureRect = $HBoxContainer/Item1/PanelContainer/Panel/VBoxContainer/TextureRect
+@onready var item1_cost_label: Label = $HBoxContainer/Item1/PanelContainer/Panel/VBoxContainer/Cost
+@onready var item1_not_in_inventory_panel: Panel = $HBoxContainer/Item1/NotInInventory
+
+@onready var item2_name_label: Label = $HBoxContainer/Item2/PanelContainer/Panel/VBoxContainer/ItemName
+@onready var item2_texture_rect: TextureRect = $HBoxContainer/Item2/PanelContainer/Panel/VBoxContainer/TextureRect
+@onready var item2_cost_label: Label = $HBoxContainer/Item2/PanelContainer/Panel/VBoxContainer/Cost
+
+@onready var item3_name_label: Label = $HBoxContainer/Item3/PanelContainer/Panel/VBoxContainer/ItemName
+@onready var item3_texture_rect: TextureRect = $HBoxContainer/Item3/PanelContainer/Panel/VBoxContainer/TextureRect
+@onready var item3_cost_label: Label = $HBoxContainer/Item3/PanelContainer/Panel/VBoxContainer/Cost
+
+@onready var item4_name_label: Label = $HBoxContainer/Item4/PanelContainer/Panel/VBoxContainer/ItemName
+@onready var item4_texture_rect: TextureRect = $HBoxContainer/Item4/PanelContainer/Panel/VBoxContainer/TextureRect
+@onready var item4_cost_label: Label = $HBoxContainer/Item4/PanelContainer/Panel/VBoxContainer/Cost
+
+@onready var item5_name_label: Label = $HBoxContainer/Item5/PanelContainer/Panel/VBoxContainer/ItemName
+@onready var item5_texture_rect: TextureRect = $HBoxContainer/Item5/PanelContainer/Panel/VBoxContainer/TextureRect
+@onready var item5_cost_label: Label = $HBoxContainer/Item5/PanelContainer/Panel/VBoxContainer/Cost
+
+@onready var item6_name_label: Label = $HBoxContainer/Item6/PanelContainer/Panel/VBoxContainer/ItemName
+@onready var item6_texture_rect: TextureRect = $HBoxContainer/Item6/PanelContainer/Panel/VBoxContainer/TextureRect
+@onready var item6_cost_label: Label = $HBoxContainer/Item6/PanelContainer/Panel/VBoxContainer/Cost
+
+@onready var item7_name_label: Label = $HBoxContainer/Item7/PanelContainer/Panel/VBoxContainer/ItemName
+@onready var item7_texture_rect: TextureRect = $HBoxContainer/Item7/PanelContainer/Panel/VBoxContainer/TextureRect
+@onready var item7_cost_label: Label = $HBoxContainer/Item7/PanelContainer/Panel/VBoxContainer/Cost
+
+@onready var result_item_name_label: Label = $HBoxContainer/ResultItem/PanelContainer/Panel/VBoxContainer/ItemName
+@onready var result_item_texture_rect: TextureRect = $HBoxContainer/ResultItem/PanelContainer/Panel/VBoxContainer/TextureRect
+@onready var result_item_cost_label: Label = $HBoxContainer/ResultItem/PanelContainer/Panel/VBoxContainer/Cost
+
+static var crafting_menu_row_packed_scene: PackedScene = preload("res://src/presentation/item/crafting/crafting_menu_row.tscn")
+
+var _item1: Item:
+	get:
+		return _item1
+	set(value):
+		_item1 = value
+		item1_name_label.text = value.name
+		item1_texture_rect.texture = value.texture
+		item1_cost_label.text = str(value.cost_price)
+
+var _item2: Item:
+	get:
+		return _item2
+	set(value):
+		_item2 = value
+		item2_name_label.text = value.name
+		item2_texture_rect.texture = value.texture
+		item2_cost_label.text = str(value.cost_price)
+
+var _item3: Item:
+	get:
+		return _item3
+	set(value):
+		_item3 = value
+		item3_name_label.text = value.name
+		item3_texture_rect.texture = value.texture
+		item3_cost_label.text = str(value.cost_price)
+
+var _item4: Item:
+	get:
+		return _item4
+	set(value):
+		_item4 = value
+		item4_name_label.text = value.name
+		item4_texture_rect.texture = value.texture
+		item4_cost_label.text = str(value.cost_price)
+
+var _item5: Item:
+	get:
+		return _item5
+	set(value):
+		_item5 = value
+		item5_name_label.text = value.name
+		item5_texture_rect.texture = value.texture
+		item5_cost_label.text = str(value.cost_price)
+
+var _item6: Item:
+	get:
+		return _item6
+	set(value):
+		_item6 = value
+		item6_name_label.text = value.name
+		item6_texture_rect.texture = value.texture
+		item6_cost_label.text = str(value.cost_price)
+
+var _item7: Item:
+	get:
+		return _item7
+	set(value):
+		_item7 = value
+		item7_name_label.text = value.name
+		item7_texture_rect.texture = value.texture
+		item7_cost_label.text = str(value.cost_price)
+
+var _result_item: Item:
+	get:
+		return _result_item
+	set(value):
+		_result_item = value
+		result_item_name_label.text = value.name
+		result_item_texture_rect.texture = value.texture
+		print("setting texture to ", value.texture)
+		result_item_cost_label.text = str(value.cost_price)
+
+
+func _ready() -> void:
+	# Reset all default item displays
+	item1_name_label.text = ""
+	item1_texture_rect.texture = null
+	item1_cost_label.text = ""
+
+	item2_name_label.text = ""
+	item2_texture_rect.texture = null
+	item2_cost_label.text = ""
+
+	item3_name_label.text = ""
+	item3_texture_rect.texture = null
+	item3_cost_label.text = ""
+
+	item4_name_label.text = ""
+	item4_texture_rect.texture = null
+	item4_cost_label.text = ""
+
+	item5_name_label.text = ""
+	item5_texture_rect.texture = null
+	item5_cost_label.text = ""
+
+	item6_name_label.text = ""
+	item6_texture_rect.texture = null
+	item6_cost_label.text = ""
+
+	item7_name_label.text = ""
+	item7_texture_rect.texture = null
+	item7_cost_label.text = ""
+
+	result_item_name_label.text = ""
+	result_item_texture_rect.texture = null
+	result_item_cost_label.text = ""
+
+
+## [code]result_item[/code]: Item to be crafted
+## [codeblock]
+##
+## Usage
+## if (result.get("success")):
+##     var crafting_menu_row_instance: CraftingMenuRow = result.get("row")
+##     rows_container.add_child(crafting_menu_row_instance)
+## else:
+##     pass
+##[/codeblock]
+static func create(recipe: Array[Item], result_item: Item) -> Dictionary:
+	print("row factory called")
+
+	# Prevents creating empty rows for items that can't be crafted
+	if recipe.is_empty():
+		return { "success": false, "row": null }
+
+	var row: CraftingMenuRow = crafting_menu_row_packed_scene.instantiate() as CraftingMenuRow
+
+	# Ensures row control nodes are fully initialized before setting items (Nill error)
+	row.call_deferred("_set_items", recipe, result_item)
+
+	return { "success": true, "row": row }
+
+
+func _set_items(recipe: Array[Item], result_item: Item) -> void:
+	print("size is", recipe.size())
+	if recipe.size() >= 1:
+		_item7 = recipe[0]
+	if recipe.size() >= 2:
+		_item6 = recipe[1]
+	if recipe.size() >= 3:
+		_item5 = recipe[2]
+	if recipe.size() >= 4:
+		_item4 = recipe[3]
+	if recipe.size() >= 5:
+		_item3 = recipe[4]
+	if recipe.size() >= 6:
+		_item2 = recipe[5]
+	if recipe.size() >= 7:
+		_item1 = recipe[6]
+
+	_result_item = result_item
