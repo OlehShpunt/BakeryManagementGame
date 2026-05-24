c9d07b59a5d3351bdf9c66485d46e42e03e82165
OlehShpunt <olehshpunt2@gmail.com>
2026-05-24 16:25:08 +0300

    feat: add crafting menu UI & open/close logic

--- bakery/src/presentation/shared/gameplay_ui.gd
+++ bakery/src/presentation/shared/gameplay_ui.gd
@@ -1,1 +1,17 @@
 extends Control
+
+@onready var crafting_menu: CraftingMenu = $CanvasLayer/CraftingMenu
+@onready var seller_ui = $CanvasLayer/SellerUi
+
+
+func _ready() -> void:
+	crafting_menu.close_crafting_menu_button_pressed.connect(_on_close_crafting_menu_pressed)
+
+
+func _on_open_crafting_menu_pressed() -> void:
+	crafting_menu.show()
+	seller_ui.hide()
+
+
+func _on_close_crafting_menu_pressed() -> void:
+	crafting_menu.hide()
